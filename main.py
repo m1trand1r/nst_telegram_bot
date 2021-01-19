f76f469d9a256e1be0896184b9eca0d0621ffcb9
m1trand1r <andrey20005647@gmail.com>
2021-01-19 22:11:33 +0500

    add webhook

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from configuration import BOT_TOKEN
+# from configuration import BOT_TOKEN
+import logging
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils.executor import start_webhook
+import os
+
+BOT_TOKEN = os.environ['BOT_TOKEN']
+
+# webhook settings
+HEROKU_APP_NAME = os.environ['HEROKU_APP_NAME']
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.environ['PORT']
 
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 
 @dp.message_handler(commands=['start'])
@@ -23,5 +39,23 @@ async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
 
 
+async def on_startup(dp: 'Dispatcher') -> None:
+    logging.warning('Starting connection. ')
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dp: 'Dispatcher') -> None:
+    logging.warning('Bye! Shutting down webhook connection')
+
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    # executor.start_polling(dp)
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
