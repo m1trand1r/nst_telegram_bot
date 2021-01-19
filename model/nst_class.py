fa88b51d7593b9063900989e6eaed39e8a6a9f54
m1trand1r <andrey20005647@gmail.com>
2021-01-19 18:50:56 +0500

    add NST model class

---
+++ model/nst_class.py
@@ -0,0 +1,124 @@
+import torchvision.models as models
+from side_functions import *
+import os
+
+
+class NST:
+    def __init__(self, path_style='D:\\python\\dls_bot\\images\\monk.jpg',
+                 path_content='D:\\python\\dls_bot\\images\\lisa.jpg'):
+        self.style_img = image_loader(path_style)
+        self.content_img = image_loader(path_content)
+        self.cnn = models.vgg19(pretrained=True).features.to(device).eval()
+        self.content_layers_default = ['conv_4']
+        self.style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+        self.normalization = Normalization(cnn_normalization_mean, cnn_normalization_std).to(device)
+        self.model = nn.Sequential(self.normalization)
+        self.content_losses = []
+        self.style_losses = []
+
+    def get_style_model_and_losses(self) -> None:
+
+        i = 0  # increment every time we see a conv
+        for layer in self.cnn.children():
+            if isinstance(layer, nn.Conv2d):
+                i += 1
+                name = 'conv_{}'.format(i)
+            elif isinstance(layer, nn.ReLU):
+                name = 'relu_{}'.format(i)
+                # The in-place version doesn't play very nicely with the ContentLoss
+                # and StyleLoss we insert below. So we replace with out-of-place
+                # ones here.
+                # Переопределим relu уровень
+                layer = nn.ReLU(inplace=False)
+            elif isinstance(layer, nn.MaxPool2d):
+                name = 'pool_{}'.format(i)
+            elif isinstance(layer, nn.BatchNorm2d):
+                name = 'bn_{}'.format(i)
+            else:
+                raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+            self.model.add_module(name, layer)
+
+            if name in self.content_layers_default:
+                # add content loss:
+                target = self.model(self.content_img).detach()
+                content_loss = ContentLoss(target)
+                self.model.add_module("content_loss_{}".format(i), content_loss)
+                self.content_losses.append(content_loss)
+
+            if name in self.content_layers_default:
+                # add style loss:
+                target_feature_1 = self.model(self.style_img).detach()
+
+                style_loss = StyleLoss(target_feature_1)
+                self.model.add_module("style_loss_{}".format(i), style_loss)
+                self.style_losses.append(style_loss)
+
+        # now we trim off the layers after the last content and style losses
+        # выбрасываем все уровни после последенего styel loss или content loss
+        for i in range(len(self.model) - 1, -1, -1):
+            if isinstance(self.model[i], ContentLoss) or isinstance(self.model[i], StyleLoss):
+                break
+
+        self.model = self.model[:(i + 1)]
+
+    def run_style_transfer(self) -> None:
+        num_steps = 800
+        style_weight = 100000
+        content_weight = 1
+        input_img = self.content_img.clone()
+
+        self.get_style_model_and_losses()
+        optimizer = get_input_optimizer(input_img)
+        run = [0]
+        while run[0] <= num_steps:
+
+            def closure():
+                # correct the values
+                # это для того, чтобы значения тензора картинки не выходили за пределы [0;1]
+                input_img.data.clamp_(0, 1)
+
+                optimizer.zero_grad()
+
+                self.model(input_img)
+
+                style_score = 0
+                content_score = 0
+
+                for sl in self.style_losses:
+                    style_score += sl.loss
+                for cl in self.content_losses:
+                    content_score += cl.loss
+
+                # взвешивание ощибки
+                style_score *= style_weight
+                content_score *= content_weight
+
+                loss = style_score + content_score
+                loss.backward()
+
+                run[0] += 1
+                if run[0] % 50 == 0:
+                    print("run {}:".format(run))
+                    print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                        style_score.item(), content_score.item()))
+                    print()
+                    imsave(input_img, style_score.item() + content_score.item())
+                    # imshow(input_img, title='step photo')
+
+                return style_score + content_score
+
+            optimizer.step(closure)
+
+        # a last correction...
+        input_img.data.clamp_(0, 1)
+
+        # return input_img
+
+    def test(self):
+        self.run_style_transfer()
+
+
+#
+# t1 = NST()
+# t1.test()
---
+++ model/side_functions.py
@@ -0,0 +1,119 @@
+from PIL import Image
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import matplotlib.pyplot as plt
+
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+import numpy as np
+
+
+import copy
+
+imsize = 256
+
+loader = transforms.Compose([
+    transforms.Resize(imsize),  # нормируем размер изображения
+    transforms.CenterCrop(imsize),
+    transforms.ToTensor()])
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+unloader = transforms.ToPILImage()
+
+
+def image_loader(image_name):
+    image = Image.open(image_name)
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
+
+class ContentLoss(nn.Module):
+
+    def __init__(self, target):
+        super(ContentLoss, self).__init__()
+            # we 'detach' the target content from the tree used
+            # to dynamically compute the gradient: this is a stated value,
+            # not a variable. Otherwise the forward method of the criterion
+            # will throw an error.
+        self.target = target.detach()  # это константа. Убираем ее из дерева вычеслений
+        self.loss = F.mse_loss(self.target, self.target)  # to initialize with something
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+
+def gram_matrix(input):
+    batch_size, h, w, f_map_num = input.size()  # batch size(=1)
+    # b=number of feature maps
+    # (h,w)=dimensions of a feature map (N=h*w)
+
+    features = input.view(batch_size * h, w * f_map_num)  # resise F_XL into \hat F_XL
+
+    G = torch.mm(features, features.t())  # compute the gram product
+
+    # we 'normalize' the values of the gram matrix
+    # by dividing by the number of element in each feature maps.
+    return G.div(batch_size * h * w * f_map_num)
+
+
+def get_input_optimizer(input_img):
+    # this line to show that input is a parameter that requires a gradient
+    # добоваляет содержимое тензора катринки в список изменяемых оптимизатором параметров
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
+    return optimizer
+
+
+class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+        self.loss = F.mse_loss(self.target, self.target)  # to initialize with something
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize img
+        return (img - self.mean) / self.std
+
+
+def imshow(tensor, title=None):
+    image = tensor.cpu().clone()
+    image = image.squeeze(0)      # функция для отрисовки изображения
+    image = unloader(image)
+    plt.imshow(image)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)
+
+
+def imsave(tensor, cnt):
+    image = tensor.cpu().clone()
+    # image = image.squeeze(0)  # функция для отрисовки изображения
+    # image = unloader(image)
+    # plt.imsave(image, 'output.png')
+    image = image[0]
+    save_image(image, f'{cnt}.png')
