ca10040a714120eaaa6528072daeb82bd4644823
m1trand1r <andrey20005647@gmail.com>
2021-01-31 00:33:21 +0500

    server version_2

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ from asgiref.sync import sync_to_async
 from nst_class import NST
 import os
 
-BOT_TOKEN = os.environ['BOT_TOKEN']
-
-# webhook settings
-HEROKU_APP_NAME = os.environ['HEROKU_APP_NAME']
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.environ['PORT']
+# BOT_TOKEN = os.environ['BOT_TOKEN']
+#
+# # webhook settings
+# HEROKU_APP_NAME = os.environ['HEROKU_APP_NAME']
+# WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+# WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+# WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+#
+# # webserver settings
+# WEBAPP_HOST = '0.0.0.0'
+# WEBAPP_PORT = os.environ['PORT']
 
 storage = MemoryStorage()
 
@@ -148,23 +148,23 @@ async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
 
 
-async def on_startup(dp: 'Dispatcher') -> None:
-    logging.warning('Starting connection. ')
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-
-
-async def on_shutdown(dp: 'Dispatcher') -> None:
-    logging.warning('Bye! Shutting down webhook connection')
+# async def on_startup(dp: 'Dispatcher') -> None:
+#     logging.warning('Starting connection. ')
+#     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+#
+#
+# async def on_shutdown(dp: 'Dispatcher') -> None:
+#     logging.warning('Bye! Shutting down webhook connection')
 
 if __name__ == '__main__':
-    # start_polling(dp, skip_updates=True)
+    start_polling(dp, skip_updates=True)
     logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    # start_webhook(
+    #     dispatcher=dp,
+    #     webhook_path=WEBHOOK_PATH,
+    #     skip_updates=True,
+    #     on_startup=on_startup,
+    #     on_shutdown=on_shutdown,
+    #     host=WEBAPP_HOST,
+    #     port=WEBAPP_PORT,
+    # )
